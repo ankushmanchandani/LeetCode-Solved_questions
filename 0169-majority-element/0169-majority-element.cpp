fb62a90e338ffaae3bc62f502934a2452d930fc5
Ankush Manchandani <53401349+ankushmanchandani@users.noreply.github.com>
2022-10-19 00:00:47 +0530

    Time: 46 ms (32.62%), Space: 19.6 MB (60.15%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> map;
+        for(auto x : nums){
+            map[x]++;
+            if(map[x] > nums.size()/2){
+                return x;
+            }
+        }
+        return 0;
+    }
+};
