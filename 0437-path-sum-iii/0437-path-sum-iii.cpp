1831d817e589ea386a5549813842980e0569cb1d
Ankush Manchandani <53401349+ankushmanchandani@users.noreply.github.com>
2022-10-20 22:40:40 +0530

    Time: 67 ms (23.38%), Space: 16 MB (82.55%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -1,23 +1,17 @@
 
 class Solution {
 public:
-    int count=0;
-    void solve(TreeNode* root,long long cursum, int k, unordered_map<long,int> &mpp){
-        if(root == NULL) return ;
-        cursum += root->val;
-        // if(cursum == k) count++;
-        if(mpp.count(cursum - k)) count += mpp[cursum-k];
-        mpp[cursum]++;
-        solve(root->left,cursum,k,mpp);
-        solve(root->right,cursum,k,mpp);
-        mpp[cursum]--;
-        cursum-=root->val;;
+    
+    int dfs(TreeNode* root, long long cursum){
+        if(root == NULL) return 0;
+        int count = 0;
+        if(root->val == cursum) count++;
+        count +=dfs(root->left,cursum - root->val);
+        count +=dfs(root->right,cursum - root->val);
+        return count;
     }
     int pathSum(TreeNode* root, int k) {
-        unordered_map<long,int> mpp;
-        long long cursum = 0;
-        mpp[0]=1;
-        solve(root,cursum,k,mpp);
-        return count;
+        if(root == NULL) return 0;
+        return   pathSum(root->left,k) + dfs(root,k) + pathSum(root->right,k);
     }
 };
