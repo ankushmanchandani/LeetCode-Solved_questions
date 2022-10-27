fc6fb6227a1206cf9fd7afe328d8591e5fb91701
Ankush Manchandani <53401349+ankushmanchandani@users.noreply.github.com>
2022-10-27 17:35:49 +0530

    Time: 0 ms (100.00%), Space: 6 MB (28.74%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0) return false;
-        while(n%2==0) n /= 2;
-        return n==1;
+        if(n<=0) return false;
+        return (ceil(log2(n)) == floor(log2(n)));
     }
 };
