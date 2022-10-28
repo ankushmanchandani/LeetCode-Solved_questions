0a7b31af848f6939a2d873bf425c49b8e7421423
Ankush Manchandani <53401349+ankushmanchandani@users.noreply.github.com>
2022-10-28 16:08:56 +0530

    Time: 51 ms (46.31%), Space: 16.4 MB (30.60%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,50 +1,32 @@
 class MinStack {
-    stack<long long> st;
-    long long mini;
+    stack<pair<int,int>>st;
+    
 public:
     MinStack() {
-        while(st.empty() == false) st.pop();
-        mini = INT_MAX;
+        
     }
     
-    void push(int value) {
-        long long val = value;
+    void push(int x) {
+        int min;
         if(st.empty()){
-            mini = val;
-            st.push(val);
+            min =x;
         }
         else{
-            if(val < mini){
-                st.push(2*val*1ll - mini);
-                mini = val;
-            }
-            else{
-                st.push(val);
-            }
+            min = std::min(st.top().second,x);
         }
+        st.push({x,min});
     }
     
     void pop() {
-        if(st.empty()) return ;
-        long long el = st.top();
         st.pop();
-        
-        if(el < mini){
-            mini = 2*mini - el;
-        }
     }
     
     int top() {
-        if(st.empty()) return -1;
-        
-        long long el = st.top();
-        if(el < mini) return mini;
-        return el;
-   
+        return st.top().first;
     }
     
     int getMin() {
-        return mini;
+        return st.top().second;
     }
 };
 
