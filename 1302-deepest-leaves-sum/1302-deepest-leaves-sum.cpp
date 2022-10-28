65a40e7d03e292e24306fc7d3432a37c2a8318af
Ankush Manchandani <53401349+ankushmanchandani@users.noreply.github.com>
2022-10-29 00:15:55 +0530

    Time: 205 ms (54.49%), Space: 61.9 MB (43.30%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root){
+        int sum=0,n;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            sum=0,n=q.size();
+            for(int i=0;i<n;i++){
+                auto top = q.front(); q.pop();
+                sum += top->val;
+                if(top->left) q.push(top->left);
+                if(top->right) q.push(top->right);
+            }
+        }
+        return sum;
+    }
+};
