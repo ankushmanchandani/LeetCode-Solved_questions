feaf3ef79ad81a963c6e0922e103225d0cd38e8b
Ankush Manchandani <53401349+ankushmanchandani@users.noreply.github.com>
2022-11-05 16:08:38 +0530

    Time: 36 ms (76.93%), Space: 6.5 MB (52.50%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -4,21 +4,21 @@ public:
         solve(board);
     }
     bool solve(vector<vector<char>>& board){
-        
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
+        for(int i=0; i<board.size(); i++){
+            for(int j=0; j<board[0].size(); j++){
                 
                 if(board[i][j] == '.'){
-                    
-                    for(char c = '1'; c <= '9' ; c++){
+                    for(char c='1'; c<='9'; c++){
+                        
                         if(isValid(board,i,j,c)){
                             board[i][j] = c;
+                            
                             if(solve(board) == true){
                                 return true;
-                            }
-                            else{
+                            }else{
                                 board[i][j] = '.';
                             }
+                            
                         }
                     }
                     return false;
@@ -27,18 +27,32 @@ public:
         }
         return true;
     }
-    bool isValid(vector<vector<char>>& board, int row, int col ,char c){
-        for(int i=0;i<9;i++){
+    bool isValid(vector<vector<char>>& board,int row, int col, char c ){
+        for(int i=0; i<9; i++){
             if(board[row][i] == c){
                 return false;
             }
             if(board[i][col] == c){
                 return false;
             }
-            if(board[3 * (row / 3) + (i /3)][3 * (col / 3) + (i % 3)] == c){
+            if(board[3 * (row/3) + (i/3)][3 * (col/3) + (i%3)] == c){
                 return false;
             }
         }
         return true;
-    }        
+    }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
