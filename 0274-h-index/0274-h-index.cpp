19a480456f4880c465cb7827c0c5b46050288588
Ankush Manchandani <53401349+ankushmanchandani@users.noreply.github.com>
2022-11-06 17:39:52 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (97.61%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int hIndex(vector<int>& c) {
+        int n = c.size();
+        sort(c.begin(),c.end());
+        int l=0, h= n-1;
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            if(c[mid] == n-mid){
+                return c[mid];
+            }
+            else if(c[mid] > n-mid){
+                h = mid-1;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        return n-l;
+    }
+};
