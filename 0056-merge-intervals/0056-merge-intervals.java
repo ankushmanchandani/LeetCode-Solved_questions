e828ab93f6f881c8421f65fb29b8d9c8ede261d6
Ankush Manchandani <53401349+ankushmanchandani@users.noreply.github.com>
2022-11-14 12:43:46 +0530

    Time: 18 ms (54.58%), Space: 55.3 MB (39.55%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,47 +1,26 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b) -> a[0]-b[0]);
-        
         List<int[]> res = new ArrayList<>();
-        res.add(intervals[0]);
-        int j=0;
+        if(intervals.length == 0 || intervals == null){
+            return res.toArray(new int[0][]);
+        }
         
-        for(int i=1;i<intervals.length;i++){
-            int[] interval = res.get(j);
-            if(intervals[i][0] <= interval[1]){
-                interval[1] = Math.max(intervals[i][1],interval[1]);
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+        
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        
+        for(int[] i : intervals){
+            if(i[0] <= end){
+                end = Math.max(end,i[1]);
             }
             else{
-                res.add(intervals[i]);
-                j++;
+                res.add(new int[]{start,end});
+                start = i[0];
+                end = i[1];
             }
         }
-        
-        int[][] ans = new int[j+1][2];
-        
-        for(int i=0;i<j+1;i++){
-            ans[i] = res.get(i);
-        }
-        return ans;
+        res.add(new int[]{start,end});
+        return res.toArray(new int[0][]);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
