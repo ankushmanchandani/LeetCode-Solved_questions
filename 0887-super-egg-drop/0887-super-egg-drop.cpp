cc413d53bb22d0a19253a23064d06cb253d529f7
Ankush Manchandani <53401349+ankushmanchandani@users.noreply.github.com>
2022-11-28 12:17:18 +0530

    Time: 806 ms (7.17%), Space: 29.6 MB (25.85%) - LeetHub

--- 0887-super-egg-drop/0887-super-egg-drop.cpp
+++ 0887-super-egg-drop/0887-super-egg-drop.cpp
@@ -1,40 +1,50 @@
 class Solution {
 public:
-    int solve(int e, int f , vector<vector<int>> &dp){
-        //base case
-        if(f == 0 || f == 1 || e == 1){
-            return f;
-        }
-        if(dp[e][f] != -1){
-            return dp[e][f];
-        }
-        int ans = INT_MAX;
-        int l=1 , h = f , temp = 0 ;
-        while(l<=h){
-            int k = (l+h)/2;
-            //egg broke
-            int left = solve(e-1, k-1,dp);
-            //egg not broke
-            int right = solve(e, f-k ,dp);
-            
-            temp = 1 + max(left,right);
-            
-            if(left < right){
-                l = k+1;
-            }
-            else{
-                h = k-1;
-            }
-            ans = min(ans,temp);
-        }
-        
-        return dp[e][f] =  ans;
-       
-    }
+ 
     int superEggDrop(int k, int n) {
         //k -> eggs
         //n -> no of floors
-        vector<vector<int>> dp(k+1, vector(n+1,-1));
-        return solve(k,n,dp);
+        if(n == 0 || n == 1){
+            return n;
+        }
+        if(k == 1){
+            return n;
+        }
+        vector<vector<int>> dp(k+1, vector(n+1,0));
+        for(int i=0; i<=k ;i++){
+            dp[i][0] = 0;
+            dp[i][1] = 1; 
+        }
+        for(int i=0; i<=n ;i++){
+            dp[0][i] = 0;
+            dp[1][i] = i;
+        }
+        for(int e=2 ; e<k+1 ;e++){
+            for(int f=2; f<n+1; f++){
+                int ans = 100000;
+                int l=1 , h = f , temp = 0 ;
+                while(l<=h){
+                    int k = (l+h)/2;
+                    //egg broke
+                    int left = dp[e-1][k-1];
+                    //egg not broke
+                    int right = dp[e][f-k];
+
+                    temp = 1 + max(left,right);
+
+                    if(left < right){
+                        l = k+1;
+                    }
+                    else{
+                        h = k-1;
+                    }
+                    ans = min(ans,temp);
+                }
+        
+                 dp[e][f] =  ans;
+            }
+        }
+        
+        return dp[k][n];
     }
 };
