f7ca9c03d4b83809af94d6a41104a0dc43607c6c
Ankush Manchandani <53401349+ankushmanchandani@users.noreply.github.com>
2022-12-01 12:02:49 +0530

    Time: 87 ms (39.26%), Space: 19.9 MB (31.03%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> mergeintervals;
+    vector<vector<int>> merge(vector<vector<int>>& interval) {
+        vector<vector<int>> mergeInteval;
+        if(interval.size() == 0) return mergeInteval;
+        sort(interval.begin() , interval.end());
+        vector<int> tempInterval= interval[0];
         
-        if(intervals.size() == 0){
-            return mergeintervals;
-        }
-        sort(intervals.begin(),intervals.end());
-        vector<int> tempinterval = intervals[0];
-        
-        for(auto it : intervals){
-            if(it[0]<=tempinterval[1]){
-                tempinterval[1] = max(it[1],tempinterval[1]);
+        for(auto it : interval){
+            if(it[0] <= tempInterval[1]){
+                tempInterval[1] = max(it[1] , tempInterval[1]);
             }
             else{
-                mergeintervals.push_back(tempinterval);
-                
-                tempinterval = it;
+                mergeInteval.push_back(tempInterval);
+                tempInterval = it;
             }
         }
-        mergeintervals.push_back(tempinterval);
-        return mergeintervals;
+        mergeInteval.push_back(tempInterval);
+        return mergeInteval;
     }
 };
