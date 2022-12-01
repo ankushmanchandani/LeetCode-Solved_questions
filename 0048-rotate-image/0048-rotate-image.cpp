faa78592a691f92704e2f6190807c9e9ca3e4ac0
Ankush Manchandani <53401349+ankushmanchandani@users.noreply.github.com>
2022-12-01 11:44:26 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (99.86%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& m) {
-        int n = m.size();
-        
+    void rotate(vector<vector<int>>& a) {
+        int n = a.size();
+        int m = a[0].size();
+        //1. transpose nikalega
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
-                swap(m[i][j],m[j][i]);
+                swap(a[i][j], a[j][i]);
             }
         }
         
+        //2. reverse kr dunha saari rows ko
         for(int i=0;i<n;i++){
-            reverse(m[i].begin(),m[i].end());
+            reverse(a[i].begin(),a[i].end());
         }
     }
 };
