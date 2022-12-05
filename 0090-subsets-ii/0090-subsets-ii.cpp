3afe431ebf71f9865fa6a2090a7cc6a7e3207d48
Ankush Manchandani <53401349+ankushmanchandani@users.noreply.github.com>
2022-12-05 22:57:49 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (70.43%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    void solve(int i , vector<int>& nums,vector<vector<int>>& powerset,vector<int>& subset){
-        if(i>=nums.size()){
-            powerset.push_back(subset);
-            return;
+    void solve(int i, vector<int>& nums , vector<vector<int>> &res , vector<int> &path){
+        if(i == nums.size()){
+            res.push_back(path);
+            return ;
         }
         
-        //pick it
-        subset.push_back(nums[i]);
-        solve(i+1,nums,powerset,subset);
-        subset.pop_back();
+        path.push_back(nums[i]);
+        solve(i+1, nums, res , path);
+        path.pop_back();
         
-        //not pick it
-        while(i+1 < nums.size() && nums[i] == nums[i+1]) i++; 
-        solve(i+1, nums, powerset, subset);
+        while(i+1<nums.size() && nums[i] == nums[i+1]) i++;
+        solve(i+1 , nums, res, path);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> powerset;
-        vector<int> subset;
-        sort(nums.begin() ,nums.end());
-        solve(0,nums,powerset,subset);
-        return powerset;
+        vector<vector<int>> res;
+        vector<int> path;
+        sort(nums.begin() , nums.end());
+        solve(0,nums,res,path);
+        return res;
     }
 };
